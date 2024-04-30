c80ffc86b75a1021ee16bfa49df14a80d9a482d3
st10068763 <anterozacarias007@gmail.com>
2024-04-30 11:08:14 +0200

    I added functionality to the focus mode button.

--- app/src/main/java/com/example/tempomaster/Settings.kt
+++ app/src/main/java/com/example/tempomaster/Settings.kt
@@ -1,18 +1,25 @@
 package com.example.tempomaster
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.tempomaster.databinding.ActivitySettingsBinding
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.graphics.Color
+import androidx.core.app.NotificationCompat
 
 
 class Settings : AppCompatActivity() {
     // Declaration of the binding variable at the class level
     private lateinit var binding: ActivitySettingsBinding
+    // notification channel ID
+    private val focusNotificationChannelId ="focus_channel"
 
     // Define the fragments to be used in your activity
     private lateinit var dashboard: Fragment
@@ -25,12 +32,16 @@ class Settings : AppCompatActivity() {
         // Initialize the binding
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Default fragment to be displayed when activity is created
-        replaceFragment(settings)
-
         // Enable edge-to-edge design
         enableEdgeToEdge()
+
+        //Creating the notification channel
+        createNotificationChannel(this)
+
+        // setting the click listener for focus mode
+        binding.focusBtn.setOnClickListener{
+        showFocusNotification(this)
+        }
 
         // Window insets listener for setting padding based on system bars
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
@@ -44,11 +55,38 @@ class Settings : AppCompatActivity() {
                 R.id.dashboardID -> replaceFragment(dashboard)
                 R.id.projectID -> replaceFragment(existingProjects)
                 R.id.settingsID -> replaceFragment(settings)
-                else -> false
             }
             true
         }
     }
+
+   private fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Focus Notification"
+            val descriptionText = "Focus mode notification"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(focusNotificationChannelId, name, importance).apply {
+                    description = descriptionText
+                    lightColor = Color.BLUE
+                }
+            // Register the notification channel to the system
+            val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+            //-----------------------------------------------------//
+            //-------------------Notifications to display----------------------//
+            private fun showFocusNotification(context: Context) {
+                val notification = NotificationCompat.Builder(context, focusNotificationChannelId)
+                        .setSmallIcon(R.drawable.focus6)
+                        .setContentTitle("Focus Mode")
+                        .setContentText("Focus mode is on")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .build()
+                //To display the notification
+                val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.notify(1, notification)
+            }
 
     // Fragment replacement method
     private fun replaceFragment(fragment: Fragment) {
@@ -59,3 +97,6 @@ class Settings : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 }
+
+
+
